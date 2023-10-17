76df82303e4aa31ddf124f0a1331a9cad6c156d7
CloudJ1603 <lujiaca16@gmail.com>
2023-10-16 22:26:28 -0700

    finished with some bugs

--- http_conn.cpp
+++ http_conn.cpp
@@ -6,6 +6,19 @@ using CHECK_STATE = http_conn::CHECK_STATE;
 using HTTP_CODE = http_conn::HTTP_CODE;
 using LINE_STATUS = http_conn::LINE_STATUS;
 
+// 定义HTTP响应的一些状态信息
+const char* ok_200_title = "OK";
+const char* error_400_title = "Bad Request";
+const char* error_400_form = "Your request has bad syntax or is inherently impossible to satisfy.\n";
+const char* error_403_title = "Forbidden";
+const char* error_403_form = "You do not have permission to get file from this server.\n";
+const char* error_404_title = "Not Found";
+const char* error_404_form = "The requested file was not found on this server.\n";
+const char* error_500_title = "Internal Error";
+const char* error_500_form = "There was an unusual problem serving the requested file.\n";
+
+// root directory of the website
+const char* doc_root = "/home/francis/Linux-Web-Server/resources";
 
 int http_conn::m_epollfd = -1;      // events from all sockets are registered to the same epoll object
 int http_conn::m_user_count = 0;    // number of users
@@ -52,7 +65,7 @@ void modfd(int epollfd, int fd, int ev) {
     epoll_event event;
     event.data.fd = fd;
     // event.events = ev | EPOLLONESHOT | EPOLLRDHUP;
-    event.events = ev | EPOLLONESHOT | EPOLLET | EPOLLRDHUP;
+    event.events = ev | EPOLLONESHOT | EPOLLRDHUP;
     epoll_ctl(epollfd, EPOLL_CTL_MOD, fd, &event);
 }
 
@@ -89,6 +102,8 @@ void http_conn::init() {
 
     // set 'READ_BUFFER_SIZE' bytes of 'm_read_buf' to 0
     bzero(m_read_buf, READ_BUFFER_SIZE);
+    bzero(m_write_buf,WRITE_BUFFER_SIZE);
+    bzero(m_real_file,FILENAME_LEN);
 }
 
 // close the connection
@@ -99,6 +114,217 @@ void http_conn::close_conn() {
         m_user_count--;
     }
 }
+
+
+// write in non-blocking mode
+bool http_conn::write()
+{
+
+    // printf("write all data at once\n");
+    // return true;
+    int temp = 0;
+    int bytes_have_send = 0;    
+    int bytes_to_send = m_write_idx;
+    
+    if ( bytes_to_send == 0 ) {
+        /*
+            Calls 'modfd' to modify the file descriptor in the epoll event to wait for incoming data.
+            This essentially sets the server up to listen for the next request from the same client
+        */
+        modfd( m_epollfd, m_sockfd, EPOLLIN ); 
+        init();     // reset the internal state of the 'http_conn' object                             
+        return true;
+    }
+
+    while(true) {
+        /*
+            Uses 'writev' to perform scatter-write operation. 'writev' is used to write data from 
+            multiple buffers ('m_iv' in this case) into a single file descriptor ('m_sockfd'). This
+            is often used for efficiency when writing data from different memory locations.
+        */
+        temp = writev(m_sockfd, m_iv, m_iv_count);
+        if ( temp <= -1 ) {  
+            /*
+                If the error is 'EAGAIN', it means that the TCP write buffer has no space at the moment, 
+                so it modifies the file descriptor in the epoll event to wait for the socket to become writable (EPOLLOUT).
+                This allows the server to wait for the next opportunity to send data when the TCP buffer has space.
+                The server cannot immidiately receive the request from the next client. 
+                If it encounters any other error, call unmap() to release any resources associated memory mapping.
+            */
+            if( errno == EAGAIN ) {
+                modfd( m_epollfd, m_sockfd, EPOLLOUT );
+                return true;
+            }
+            unmap();
+            return false;
+        }
+
+        bytes_to_send -= temp;
+        bytes_have_send += temp;
+        if ( bytes_to_send <= bytes_have_send ) {
+            unmap();
+            /*
+                Depending on the value of m_linger, it decides whether to keep the connection open for potential 
+                future request or close it.
+                    - m_linger is true: call init() to reset the internal state, modify the epoll event to wait for incoming data
+                    - m_linger is false: modify the epoll event to wait for incoming data, return false to indicate the connection should be closed
+            */
+            if(m_linger) {
+                init();
+                modfd( m_epollfd, m_sockfd, EPOLLIN );
+                return true;
+            } else {
+                modfd( m_epollfd, m_sockfd, EPOLLIN );
+                return false;
+            } 
+        }
+    }
+}
+
+// unmap the memory mapping 
+void http_conn::unmap() {
+    if( m_file_address )
+    {
+        munmap( m_file_address, m_file_stat.st_size );
+        m_file_address = 0;
+    }
+}
+
+// Generate an HTTP response based on the given HTTP_CODE (the result of processing the request)
+bool http_conn::process_write(HTTP_CODE ret) {
+    // set up the status line, headers and content (if appicable) for the response
+    switch (ret)
+    {
+        case INTERNAL_ERROR:
+            add_status_line( 500, error_500_title );
+            add_headers( strlen( error_500_form ) );
+            if ( ! add_content( error_500_form ) ) {
+                return false;
+            }
+            break;
+        case BAD_REQUEST:
+            add_status_line( 400, error_400_title );
+            add_headers( strlen( error_400_form ) );
+            if ( ! add_content( error_400_form ) ) {
+                return false;
+            }
+            break;
+        case NO_RESOURCE:
+            add_status_line( 404, error_404_title );
+            add_headers( strlen( error_404_form ) );
+            if ( ! add_content( error_404_form ) ) {
+                return false;
+            }
+            break;
+        case FORBIDDEN_REQUEST:
+            add_status_line( 403, error_403_title );
+            add_headers(strlen( error_403_form));
+            if ( ! add_content( error_403_form ) ) {
+                return false;
+            }
+            break;
+        case FILE_REQUEST:
+            add_status_line(200, ok_200_title );
+            add_headers(m_file_stat.st_size);
+            // sets up two 'iovec' structure to send both the headers and the file content
+            m_iv[ 0 ].iov_base = m_write_buf;         // header
+            m_iv[ 0 ].iov_len = m_write_idx;          // length of the header
+            m_iv[ 1 ].iov_base = m_file_address;      // file content
+            m_iv[ 1 ].iov_len = m_file_stat.st_size;  // length of the file content
+            m_iv_count = 2;
+            return true;
+        default:
+            return false;
+    }
+
+    // set up m_iv to contain only the header
+    m_iv[ 0 ].iov_base = m_write_buf;
+    m_iv[ 0 ].iov_len = m_write_idx;
+    m_iv_count = 1;
+    return true;
+}
+
+/*
+    The following functions are called by process_write() to generate HTTP response
+        void unmap();
+        bool add_response( const char* format, ... );
+        bool add_content( const char* content );
+        bool add_content_type();
+        bool add_status_line( int status, const char* title );
+        bool add_headers( int content_length );
+        bool add_content_length( int content_length );
+        bool add_linger();
+        bool add_blank_line();   
+*/ 
+
+bool http_conn::add_status_line( int status, const char* title ) {
+    return add_response( "%s %d %s\r\n", "HTTP/1.1", status, title );
+}
+
+bool http_conn::add_headers(int content_len) {
+    // add_content_length(content_len);
+    // add_content_type();
+    // add_linger();
+    // add_blank_line();
+    return add_content_length(content_len) && add_content_type()
+            && add_linger() && add_blank_line();
+}
+
+bool http_conn::add_content_length(int content_len) {
+    return add_response( "Content-Length: %d\r\n", content_len );
+}
+
+bool http_conn::add_content_type() {
+    // FUTURE IMPROVEMENTS: extend the content type here
+    return add_response("Content-Type:%s\r\n", "text/html");
+}
+
+bool http_conn::add_linger() {
+    return add_response( "Connection: %s\r\n", ( m_linger == true ) ? "keep-alive" : "close" );
+}
+
+bool http_conn::add_blank_line() {
+    return add_response( "%s", "\r\n" );
+}
+
+bool http_conn::add_content( const char* content ) {
+    return add_response( "%s", content );
+}
+
+// add a response to the server's write buffer
+bool http_conn::add_response( const char* format, ... ) {
+    // ensure the current position within the write buffer does not exceed the maximum write buffer size
+    if( m_write_idx >= WRITE_BUFFER_SIZE ) {
+        return false;
+    }
+    
+    /*
+        va_list: This is a data type that represents a list of variable arguments
+
+        va_start(): initialize arg_list to point to the first optional argument ('format' in this case) 
+        after which additional arguments can be accessed.
+
+        vsnprinf(): variable-argument 'snprintf'. It is used for formatting and writing data to a string buffer
+        while allowing for variable-length argument list.
+            - m_write_buf + m_write_index: destination buffer m_write_buf with starting position m_write_index
+            - WRITE_BUFFER_SIZE - 1 - m_write_index: the max number of characters that can be written to the buffer
+            - format: This is a format string that specifies how the data should be formatted. 
+            - arg_list: This is a 'va_list' object
+
+    */
+    va_list arg_list;                 
+    va_start( arg_list, format );     
+    int len = vsnprintf( m_write_buf + m_write_idx, WRITE_BUFFER_SIZE - 1 - m_write_idx, format, arg_list );
+    if( len >= ( WRITE_BUFFER_SIZE - 1 - m_write_idx ) ) {
+        return false;
+    }
+    m_write_idx += len;
+    va_end( arg_list );   // clean up the 'arg_list"
+    return true;
+}
+
+
+
 
 // read in non-blocking mode, keep looping until there is no more data or the connection is closd
 bool http_conn::read() {
@@ -136,11 +362,6 @@ bool http_conn::read() {
     return true;
 }
 
-// write in non-blocking mode
-bool http_conn::write() {
-    printf("write all data at once\n");
-    return true;
-}
 
 // the main state machine
 HTTP_CODE http_conn::process_read() {
@@ -192,6 +413,7 @@ HTTP_CODE http_conn::process_read() {
     }
     return NO_REQUEST;
 }
+
 /*
     The following function will be called by process_read() to parse HTTP request  
     HTTP_CODE parse_request_line( char* text );
@@ -240,9 +462,8 @@ HTTP_CODE http_conn::parse_request_line( char* text ) {
     /*
         /index.html HTTP/1.1
         Find the first occurrence of either a space of a tab in the string 
-
     */
-    // 检索字符串 str1 中第一个不在字符串 str2 中出现的字符下标。
+
     m_version = strpbrk( m_url, " \t" );
     if (!m_version) {
         return BAD_REQUEST;
@@ -251,7 +472,7 @@ HTTP_CODE http_conn::parse_request_line( char* text ) {
     if (strcasecmp( m_version, "HTTP/1.1") != 0 ) {
         return BAD_REQUEST;
     }
-    /**
+    /*
      * http://192.168.110.129:10000/index.html
     */
     if (strncasecmp(m_url, "http://", 7) == 0 ) {   
@@ -262,23 +483,91 @@ HTTP_CODE http_conn::parse_request_line( char* text ) {
     if ( !m_url || m_url[0] != '/' ) {
         return BAD_REQUEST;
     }
-    m_check_state = CHECK_STATE_HEADER; // 检查状态变成检查头
+    m_check_state = CHECK_STATE_HEADER; // update the state to CHECK_STATE_HEADER
     return NO_REQUEST;
 }
 
-HTTP_CODE http_conn::parse_headers( char* text ) {
-    return http_conn::NO_REQUEST;
+// parse HTTP request header
+HTTP_CODE http_conn::parse_headers(char* text) {   
+    // Encounter null character (a blank line), indicating the parsing of header is finished
+    if( text[0] == '\0' ) {    
+        // If there exists HTTP message body, then read the message of length 'm_content_length'
+        // transfer the state of the maine state machine to CEHCK_STATE_CONTENT
+        if ( m_content_length != 0 ) {
+            m_check_state = CHECK_STATE_CONTENT;
+            return NO_REQUEST;
+        }
+        // else, all conponents of HTTP request have been parsed
+        return GET_REQUEST;
+    } else if ( strncasecmp( text, "Connection:", 11 ) == 0 ) {  // parsing HTTP header
+        // handle connection, for example: 'Connection: keep-alive'
+        text += 11;
+        text += strspn( text, " \t" );
+        if ( strcasecmp( text, "keep-alive" ) == 0 ) {
+            m_linger = true;
+        }
+    } else if ( strncasecmp( text, "Content-Length:", 15 ) == 0 ) {
+        // handle Content-Length, for example: "Content-Length: 1000"
+        text += 15;
+        text += strspn( text, " \t" );
+        m_content_length = atol(text);
+    } else if ( strncasecmp( text, "Host:", 5 ) == 0 ) {
+        // handle Host, for example: 'Host: 192.168.193.128:10000'
+        text += 5;
+        text += strspn( text, " \t" );
+        m_host = text;
+    } else {
+        printf( "Unknow Header %s\n", text );
+    }
+    return NO_REQUEST;
 }
 
+// parse HTTP request content
 HTTP_CODE http_conn::parse_content( char* text ) {
-    return http_conn::NO_REQUEST;
+    if ( m_read_index >= ( m_content_length + m_checked_index ) )
+    {
+        text[ m_content_length ] = '\0';
+        return GET_REQUEST;
+    }
+    return NO_REQUEST;
 }
 
-HTTP_CODE http_conn::do_request() {
-    return http_conn::NO_REQUEST;
+/*
+    When receiving a complete and valid HTTP request, we will analyze the attributes of the target file.
+    If the target file exists, and readable by all users, and is not a directory, we use mmap to map it to
+    the memory address at m_file_address, and inform the caller that the file retrieval was successful.
+*/
+HTTP_CODE http_conn::do_request()
+{
+    // "/home/nowcoder/webserver/resources"
+    strcpy( m_real_file, doc_root );  // copy the root directory path to the m_real_file buffer
+    int len = strlen( doc_root );     // calculate the length of the root directory path
+    // append the requested URL. It ensures that the resulting path does not exceed the buffer size.
+    strncpy( m_real_file + len, m_url, FILENAME_LEN - len - 1 );
+    // fetch m_real_file related info，-1: failure，0: success
+    if ( stat( m_real_file, &m_file_stat ) < 0 ) {
+        return NO_RESOURCE;
+    }
+
+    // check access permissions
+    if ( ! ( m_file_stat.st_mode & S_IROTH ) ) {
+        return FORBIDDEN_REQUEST;
+    }
+
+    // check if it is a directory
+    if ( S_ISDIR( m_file_stat.st_mode ) ) {
+        return BAD_REQUEST;
+    }
+
+    // open the file in read-only mode
+    int fd = open( m_real_file, O_RDONLY );
+    // establish memory mapping
+    m_file_address = ( char* )mmap( 0, m_file_stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0 );
+    close( fd );
+    return FILE_REQUEST;
 }
 
-// 
+// parse one line
 LINE_STATUS http_conn::parse_line() {
     char temp;
     for ( ; m_checked_index < m_read_index; ++m_checked_index ) {
--- http_conn.h
+++ http_conn.h
@@ -21,10 +21,9 @@
 
 class http_conn {
 public:
-    // all events on the sockets are registered under the same epoll object
-    static int m_epollfd;  
+   
+    static int m_epollfd;      // all events on the sockets are registered under the same epoll object
     static int m_user_count;   // number of users
-
     static const int FILENAME_LEN = 200;         // the maximum length of filename
     static const int READ_BUFFER_SIZE = 2048;    // read buffer size
     static const int WRITE_BUFFER_SIZE = 1024;   // write buffer size
@@ -64,52 +63,90 @@ public:
     http_conn() {};
     ~http_conn() {};
 
-    void process();  // process request from client end
-    
     void init(int sockfd, const sockaddr_in& addr);  // initialize new connection
     void close_conn();   // close connection
     bool read();   // read in non-blocking mode
     bool write();  // write in non-blocking mode
-
+    void process();  // process request from client end
     
 
 
 
 private:
     int m_sockfd;            // the socket connected with this HTTP
-    sockaddr_in m_address;  // the address of the socket
+    sockaddr_in m_address;   // the address of the socket
+    CHECK_STATE m_check_state;  // the current state of the main state machine
 
+    // ---------------------------- read related variables ------------------------------
     char m_read_buf[READ_BUFFER_SIZE];   // read buffer
-    int m_read_index;    // the position right AFTER the last byte of the content in read buffer
+    int m_read_index;      // the position right AFTER the last byte of the content in read buffer
 
     int m_checked_index;   // the position of the byte being parsed currently
-    int m_start_line;     // the starting position of the line being parsed currently
+    int m_start_line;      // the starting position of the line being parsed currently
     
-    // HTTP request line
-    char* m_url;        // requested resource path
-    char* m_version;    // HTTP version
-    METHOD m_method;    // HTTP method
+    // HTTP status line
+    char* m_url;            // requested resource path
+    char* m_version;        // HTTP version
+    METHOD m_method;        // HTTP method
+    char* m_host;           // the target host and the port where the request is being sent
+    bool m_linger;          // it suggests whether the client would like to keep the connection open for potential further request
 
-    char* m_host;    // the target host and the port where the request is being sent
-    bool m_linger;   // it suggests whether the client would like to keep the connection open for potential further request
+    // HTTP header
+    int m_content_length;   // the length of HTTP request content
+    
+    // do_request
+    char m_real_file[FILENAME_LEN];     // the complete path of the file requested by the client
+
+             
+
+    // ---------------------------- write related variables ------------------------------
+    char m_write_buf[ WRITE_BUFFER_SIZE ];  // write buffer
+    int m_write_idx;                        // the number of bytes need to write in the buffer
+    char* m_file_address;                   // the starting point for the target file requested by the client 'mmapped' in memory
+    /*
+        the state of the target file. we can determine:
+            - the existance of teh file
+            - whether it is a directory or not
+            - whether it is readable or not
+            - the size of the file
+    */ 
+    struct stat m_file_stat;                
+    struct iovec m_iv[2];                   
+    int m_iv_count;                        // the number of memory block being written
 
 
-    CHECK_STATE m_check_state;  // the current state of the main state machine
+
 
 
     void init();      // 初始化连接
     
-    bool process_write( HTTP_CODE ret );    // fill http response  
+     
 
 
     HTTP_CODE process_read();    // parse http request - the main state machine
-    // The following function will be called by process_read() to parse HTTP request      
+    // The following functions are called by process_read() to parse HTTP request      
     HTTP_CODE parse_request_line( char* text );
     HTTP_CODE parse_headers( char* text );
     HTTP_CODE parse_content( char* text );
     HTTP_CODE do_request();
     LINE_STATUS parse_line();
     char* get_line() { return m_read_buf + m_start_line; }
+
+    bool process_write( HTTP_CODE ret );    // generate http response 
+    void unmap();
+    // The following functions are called by process_write() to generate HTTP response
+    
+    bool add_status_line( int status, const char* title );
+    bool add_headers( int content_length );
+        // add_content_length, add_content_type, add_linger, add_blank_line are called by add_header
+    bool add_content_length( int content_length );
+    bool add_content_type();
+    bool add_linger();
+    bool add_blank_line();   
+    bool add_content( const char* content );
+    bool add_response( const char* format, ... );
+    
+
 };
 
 
